685b326a0118d7d8bc15dddedcc06a236bdf7f7f
Diaszano <lucasdiiassantos@gmail.com>
2022-06-23 02:15:05 -0300

    projeto iniciado

---
+++ banco/__init__.py
@@ -0,0 +1,22 @@
+"""Modulo do Banco de Dados"""
+#-----------------------
+# BIBLIOTECAS
+#-----------------------
+from .database import User
+from .database import Insert_user
+from .databaseSqlite import DataBaseSqlite
+#-----------------------
+# CONSTANTES
+#-----------------------
+#-----------------------
+# CLASSES
+#-----------------------
+#-----------------------
+# FUNÇÕES()
+#-----------------------
+#-----------------------
+# Main()
+#-----------------------
+if(__name__ == "__main__"):
+    pass; 
+#-----------------------
